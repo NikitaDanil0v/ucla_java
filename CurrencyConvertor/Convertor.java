316786e2bb35357da8f2edc5c5fd084e036bdbd8
Nikita Danilov <Nikita Danilov>
2017-08-17 18:41:38 -0700

    Final Project

---
+++ CurrencyConvertor/Convertor.java
@@ -0,0 +1,63 @@
+package CurrencyConvertor;
+
+/**
+ * Created by Nikit on 17.08.2017.
+ */
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Convertor {
+
+    public HashMap<String, Float> readCSV (String csvFilePath, String delimiter) {
+        HashMap<String, Float> h = new HashMap<String, Float>();
+
+        try {
+            Scanner in = new Scanner(new FileReader(csvFilePath));
+            while(in.hasNextLine())
+            {
+                String[] s = in.nextLine().split(delimiter);
+
+                // Doesn't work without SUBSTRING. It seems to me that first element of array has hidden chars.
+                String key = String.valueOf(s[0]).substring(0,3);
+                Float value = Float.valueOf(s[2]);
+
+                h.put(key, value);
+            }
+        } catch (Exception e){
+            System.err.println("Error " + e.getMessage());
+        } finally {
+            return h;
+        }
+    }
+
+    public static void main(String args[]){
+        try{
+            System.out.println("====== Currency Convertor ======");
+            Convertor convertor = new Convertor();
+            HashMap<String, Float> dbCurrency = convertor.readCSV("src/CurrencyConvertor/forex.csv", ",");
+
+            Scanner s = new Scanner(System.in);
+
+            System.out.println("Enter your first currency (ex:USD)");
+            String curr1 = s.next().toUpperCase();
+
+            System.out.println("Enter amount of " + curr1);
+            Integer amt1 = s.nextInt();
+
+            System.out.println("Enter your second currency (ex:MXN)");
+            String curr2 = s.next();
+
+            Float cross_course =  dbCurrency.get(curr2) / dbCurrency.get(curr1) ;
+            Float amt2 = amt1 * cross_course;
+
+            System.out.println("Cross course:" + cross_course + " " + curr1 + "/" + curr2);
+            System.out.println(amt1 + " " + curr1 + " = " + amt2 + " " + curr2);
+
+        } catch (Exception e){
+            System.err.println("Error " + e.getMessage());
+        }
+    }
+}
